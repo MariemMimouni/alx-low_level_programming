7fd0ebec0c657e67f8a02d3075afe71bda510369
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-09 12:31:16 -0700

    2-task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#import <stdio.h>
+#import <cstdio.h>
+/**
+ * main -main block
+ * Description: print lowercase letters 
+ * Return 0
+ */
+int main (void)
+{
+	int n;
+
+	for ( n=97 ; n < 123 ; n++ )
+	{
+		putchar(n"\n");
+	}
+}
