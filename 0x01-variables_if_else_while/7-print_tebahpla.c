1b01d43ad678634f10e9447619f890ec6f040b8f
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-09 13:38:15 -0700

    5-task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print letters in reverse
+ * REturn: 0
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 122; i >98 ;i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
