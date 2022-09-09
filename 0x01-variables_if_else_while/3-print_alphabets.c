e1e9f68ff4518cb14ecdddb4a684fc3077c3c272
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-09 12:55:03 -0700

    3-task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main -main block
+ * Description: print letters in lowercase then uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	int y;
+
+	for (x = 97; x < 123; x++)
+	{
+		putchar(x);
+	}
+	for (y = 65; y < 91; y++)
+	{
+		putchar(y);
+	}
+	putchar(10);
+	return(0);
+}
+	
