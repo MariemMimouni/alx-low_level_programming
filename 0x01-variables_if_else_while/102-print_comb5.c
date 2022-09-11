c0e4a23080f439f3a1fffd1de20da3a404e02b8c
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-11 13:21:56 -0700

    task102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,10 @@ int main(void)
 	{
 		for (i2 = 48; i2 < 58; i2++)
 		{
-			x = i2 + 1;
+			if (i2 == 57)
+				x = 48;
+			else
+				x = i2 + 1;
 			for (j1 = i1; j1 < 58; j1++)
 			{
 				for (j2 = x; j2 < 58; j2++)
