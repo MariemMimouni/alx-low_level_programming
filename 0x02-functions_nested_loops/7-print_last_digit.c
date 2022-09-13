6cddbd2225184d7dc12f8c493228bbc34ff41509
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-13 14:23:11 -0700

    7-task

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit -block
+ * @n: parameter
+ * Description: print last digit
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int x = n % 10;
+
+	return (x);
+}
