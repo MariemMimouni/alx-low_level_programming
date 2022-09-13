31aa4492ca2a301688315652ff3104db59e9f0ae
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-13 12:34:57 -0700

    2-task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - block
+ * Description: print aphabet x10
+ */
+void print_alphabet_x10(void)
+{
+	int i = 1;
+
+	while (i <= 10)
+	{
+		print_alphabet();
+	}
+}
