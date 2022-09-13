c8b754530fbb3f2262c5050e678082e02057a61a
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-13 12:16:13 -0700

    1-task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - block
+ * Description:write print_alphabet function
+ * Return: 0;
+ */
+void print_alphabet(void)
+{
+	int i = 'a';
+
+	while(i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('/n');
+	return (0);
+}
