474cf31ccfcb1dac3aaf34a21ec33da65a1d59b7
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-13 15:10:08 -0700

    8-task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -22,14 +22,15 @@ void jack_bauer(void)
 			{
 				for (m2 = 0; m2 <= 9; m2++)
 				{
-					if (h1 == 2 && h2 == 3 && m1 == 5 && m2 == 9)
-						break;
-					_putchar('0' + h1);
-					_putchar('0' + h2);
-					_putchar(':');
-					_putchar('0' + m1);
-					_putchar('0' + m2);
-					_putchar('\n');
+					if !(h1 == 2 && h2 == 3 && m1 == 5 && m2 == 9)
+					{
+						_putchar('0' + h1);
+						_putchar('0' + h2);
+						_putchar(':');
+						_putchar('0' + m1);
+						_putchar('0' + m2);
+						_putchar('\n');
+					}
 				}
 			}
 		}
