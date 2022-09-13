8e3212a7b161f6e1168add345cbe92b25bd3f3a9
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-13 16:32:57 -0700

    11-task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,33 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n > 98)
 	{
-		printf("%d", i);
-		if (i < 98)
+		for (i = 98; i <= n; i++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf("%d", i);
+			if (i < 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+	}
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	if (n == 98)
+	{
+		printf("%d", n);
 	}
 	_putchar('\n');
 }
