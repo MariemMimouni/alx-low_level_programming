c827537cfaa7a6b82315c8dc42e99fcd7dfc9354
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-13 16:01:40 -0700

    9-task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,22 +6,26 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int r, c, d;
 
-	for (i = 0; i <= 9; i++)
+	for (r = 0; r <= 9; r++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 1; c <= 9; c++)
 		{
-			if ((i * j) % 10 == 0 && !((i *j) / 10 == 0))
-				_putchar(' ');
+			d = r * c;
+			if ((d / 10)>0)
+				_putchar(d / 10 + '0');
 			else
-			{
 				_putchar(' ');
+			_putchar('0' + d % 10);
+			if (c < 9)
+			{
+				_putchar(',');
 				_putchar(' ');
 			}
-			_putchar('0' + (i * j) / 10);
-			_putchar('0' + (i * j) % 10);
-			_putchar(',');
 		}
 		_putchar('\n');
 	}
