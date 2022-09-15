0c789f3ca0ad2dcbf34d2611a173e554cc1f0b47
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-15 13:28:56 -0700

    6-task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - block
+ * Description: print line
+ * @n: parameter
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
