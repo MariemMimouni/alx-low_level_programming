6b336b0405962822e01e8f60ad1f3b776e12c7ac
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-15 13:37:42 -0700

    7-task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_diagonal - block
+ * @n: parameter
+ * Description: print diagonal length n
+ * Return: none
+ */
+void print_digital(int n)
+{
+	int i = 0;
+
+	if (n != 0)
+	{
+		while (i < n)
+			_putchar(' ');
+		_putchar('\');
+	}
+	_pucthar('\n')
