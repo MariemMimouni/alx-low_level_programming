226f19ebc03e7717dd3f1a49aa18bd90a661c089
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-15 15:05:25 -0700

    9-task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,16 +11,27 @@ int main(void)
 	while (i <= 100)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
+		{
 			printf("FizzBuzz");
+		}
 		else if (i % 5 == 0)
+		{
 			printf("Buzz");
+		}
 		else if (i % 3 == 0)
+		{
 			printf("Fizz");
+		}
 		else
+		{
 			printf("%d", i);
+		}
 		i++;
 		if (i != 100)
+		{
 			putchar(' ');
+		}
 	}
 	putchar('\n');
+	return (0);
 }
