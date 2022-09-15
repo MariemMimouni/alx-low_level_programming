5cb0b9f351c3b922333e3b3f7c458152ee918d97
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-15 10:20:23 -0700

    4-task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - block
+ * Description: print numbers exept 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+		if (i == 2 || i == 4)
+			continue;
+	}
+	_putchar('\n');
+}
