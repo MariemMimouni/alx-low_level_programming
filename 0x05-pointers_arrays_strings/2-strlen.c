bde991a4665ba64e1ee796e507ea4ca515ca5427
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-19 06:46:57 -0700

    2-task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * int_strlen - block
+ * Description: compute the length of a string
+ * @s:  parameter of type pointer
+ * Return: length of string
+ */
+int int_strlen(char *s)
+{
+	int i = 0;
+
+	while (*s[i] != '\0')
+	{
+		i++;
+	}
+	return (i + 1);
+}
+
+
