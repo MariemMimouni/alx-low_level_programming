1c62d6352bf7964dd46b397d3dfee85f954fd9be
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-19 06:21:56 -0700

    0-task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - block
+ * Description: change value of var using pointer
+ * @n: parameter
+ * Return: none
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+C- pointers, arrays ,and strings
+										        }
+
