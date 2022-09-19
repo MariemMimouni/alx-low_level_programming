4e8f009dbbfc6c05654e2986757b2fabbeeb1f93
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-19 10:46:40 -0700

    9-task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strcpy - block
+ * Description: copy string to another one and return address
+ * @dest: parameter1 destination
+ * @src: parameter2 source
+ * Return: address of the destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr;
+
+	int l = 0;
+
+	int i;
+
+	while (src[l])
+		l++;
+	for (i = 0; i < l; i++)
+	{
+		dest[i] = src[i];
+	}
+	ptr = dest;
+	return (ptr);
+}
