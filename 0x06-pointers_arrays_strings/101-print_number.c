e950a947d7b3603491047dd63dc907f434e66994
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-22 02:34:04 -0700

    ATasks

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_number - block
+ * Description: print number using putchar
+ * @n: parameter
+ * Return: none
+ */
+void print_number(int n)
+{
+	int i = 10;
+
+	while (n / i != 0)
+	{
+		x = n % i
