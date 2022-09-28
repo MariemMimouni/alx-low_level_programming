4016550730f3b8b3196588b57a8050d1faca434c
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-27 21:49:41 -0700

    0-task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - block
+ * Description: print string followed by new line
+ * @s: parameter1
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+		_putchar('\n');
+	else
+		_puts_recursion(*(s + 1));
+}
