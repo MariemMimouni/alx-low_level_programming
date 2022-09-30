c3d829c3af6eef05935a6366ba37f20c1b061de9
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-30 05:44:46 -0700

    1-task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - block
+ * Description: print number of arguments
+ * @argc: arg1
+ * @argv: arguments array
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
