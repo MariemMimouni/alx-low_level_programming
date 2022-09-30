c726a7d5c948a9f1b467f7abf2c921c8c138ca66
MariemMimouni <mariemmimouni800@gmail.com>
2022-09-30 05:36:56 -0700

    0-task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - block
+ * Description: print name
+ * @argc: arg1
+ * @argv: arguments array
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
