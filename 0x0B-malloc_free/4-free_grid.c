236f42fbd622a3635788d5747e39689e8f725961
MariemMimouni <mariemmimouni800@gmail.com>
2022-10-03 04:40:35 -0700

    4-task

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * free_grid - free 2 dimentional grid
+ * @grid: parameter1
+ * @height : paramter2
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int j;
+
+	if (grid != NULL && height != 0)
+	{
+		for (j = 0; j < height; j++)
+			free(grid[i]);
+		free(grid);
+	}
+}
