51f40f1eff82288204e0c93666e72d99f15d3506
MariemMimouni <mariemmimouni800@gmail.com>
2022-10-03 04:22:35 -0700

    3-task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,7 +27,12 @@ int **alloc_grid(int width, int height)
 	{
 		array[i] = malloc(sizeof(int) * width);
 		if (array[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(array[i]);
+			free(array);
 			return (NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{
