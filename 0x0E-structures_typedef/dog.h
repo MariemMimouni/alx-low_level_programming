8ad12a342dc11116f771d2c4378c4facef5ad1b2
MariemMimouni <mariemmimouni800@gmail.com>
2022-10-10 11:27:27 -0700

    0-task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_def
+#define dog_def
+/**
+ * struct dog - datatype dog
+ * @name: char pointer
+ * @age: float
+ * @owner: char pointer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
