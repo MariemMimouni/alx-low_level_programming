96dc914e8dc11ded133f11d5c02fac473f19da9c
MariemMimouni <mariemmimouni800@gmail.com>
2022-10-10 12:05:48 -0700

    task2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef _DOG
+#define _DOG
 /**
  * struct dog - struct that stores some information of a dog
  * @name: name of the dog
@@ -15,4 +15,5 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
