ee62c65fa77496c5fbb0df29708b43fc1f1bd570
MariemMimouni <mariemmimouni800@gmail.com>
2022-10-13 15:11:04 -0700

    1-task

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,6 +22,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i != (n - 1) && separator != NULL)
 			print("%s", separator);
 	}
-	printf('\n');
+	printf("\n");
 	va_end(ptr);
 }
